c5d38f8f1422f8ae0efd13fd7737063f2349edf6
lilToucan <ivancamuzzi@gmail.com>
2026-08-04 18:20:40 +0200

    forgot to add stuff with new plugin
    changed Conditions to use the new plugin

--- Assets/Scripts/Progression/Conditions.cs
+++ Assets/Scripts/Progression/Conditions.cs
@@ -1,8 +1,9 @@
+using AYellowpaper.SerializedCollections;
+
 namespace VNMaker.Progression
 {
-    [System.Serializable]
-    public class Conditions : SerializableDictionaryBase<ConditionKeys, int> 
+    [System.Serializable()]
+    public class Conditions : SerializedDictionary<ConditionKeys, int>
     {
-        
     }
 }
